36f6d395f87eb276897e73c39f06d901938537a5
mwaiyusuf <mwaigalo5@gmail.com>
2019-02-22 20:30:06 +0300

    event

--- js/custom.js
+++ js/custom.js
@@ -1,8 +1,10 @@
 $(document).ready(function(){
-    var que1=parseInt($("input[name='']").val());
-    var que1=parseInt($("input[name='']").val());
-    var que1=parseInt($("input[name='']").val());
-    var que1=parseInt($("input[name='']").val());
-    document.getElementById('').innerHTML="Score is" + total;
+    Event.preventDefault();
+    var que1=parseInt($("input[name='option']").val());
+    var que2=parseInt($("input[name='option']").val());
+    var que3=parseInt($("input[name='option']").val());
+    var que4=parseInt($("input[name='option']").val());
+       var total = que1 + que2 + que3 + que4;
+    document.getElementById('tot').innerHTML="Score is" + total;
     
 })
