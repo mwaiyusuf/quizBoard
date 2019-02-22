49f36320cbd1601902744051f729b65d7fca9b53
mwaiyusuf <mwaigalo5@gmail.com>
2019-02-22 20:38:58 +0300

    checked in js

--- js/custom.js
+++ js/custom.js
@@ -1,9 +1,9 @@
 $(document).ready(function(){
     Event.preventDefault();
-    var que1=parseInt($("input[name='option']").val());
-    var que2=parseInt($("input[name='option']").val());
-    var que3=parseInt($("input[name='option']").val());
-    var que4=parseInt($("input[name='option']").val());
+    var que1=parseInt($("input:radio[name=query1]:checked").val());
+    var que2=parseInt($("input:radio[name=query2]:checked").val());
+    var que3=parseInt($("input:radio[name=query3]:checked").val());
+    var que4=parseInt($("input:radio[name=query4]:checked").val());
        var total = que1 + que2 + que3 + que4;
     document.getElementById('tot').innerHTML="Score is" + total;
     
