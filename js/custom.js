da7ea495eb4dac85d6b5bf81b17366b1f8e94893
mwaiyusuf <mwaigalo5@gmail.com>
2019-02-22 20:06:56 +0300

    js

---
+++ js/custom.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    var que1=parseInt($("input[name='']").val());
+    var que1=parseInt($("input[name='']").val());
+    var que1=parseInt($("input[name='']").val());
+    var que1=parseInt($("input[name='']").val());
+    document.getElementById('').innerHTML="Score is" + total;
+    
+})
