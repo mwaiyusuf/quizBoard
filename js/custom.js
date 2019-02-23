d6b6d3012e530f3d3dc32fd4263128db6187b4c4
mwaiyusuf <mwaigalo5@gmail.com>
2019-02-23 14:00:41 +0300

    click function

--- js/custom.js
+++ js/custom.js
@@ -1,10 +1,14 @@
 $(document).ready(function(){
-    Event.preventDefault();
+    $("form#qst").submit(function(event){
+    event.preventDefault();
+        alert("Works");
     var que1=parseInt($("input:radio[name=query1]:checked").val());
     var que2=parseInt($("input:radio[name=query2]:checked").val());
     var que3=parseInt($("input:radio[name=query3]:checked").val());
     var que4=parseInt($("input:radio[name=query4]:checked").val());
        var total = que1 + que2 + que3 + que4;
-    document.getElementById('tot').innerHTML="Score is" + total;
+//    document.getElementById('tot').innerHTML="Score is" + total;
+    alert(total);
+    });
     
-})
+});
